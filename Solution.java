391247db33c595845420293c47985b6884485931
Amit Kawad <37235516+AmitKawad@users.noreply.github.com>
2018-03-17 10:30:55 +0530

    Add files via upload
    Day 2 of HackerRank-30-days-of-code
    Operators

---
+++ Solution.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double meal_cost = in.nextDouble();
+        int tip_percent = in.nextInt();
+        int tax_percent = in.nextInt();
+        in.close();
+        double per=100;
+        double tip,tax;
+        int sum;
+        tip=(tip_percent/per)*meal_cost;
+        tax=(tax_percent/per)*meal_cost;
+        
+        sum=(int)Math.round(meal_cost+tax+tip);
+        
+        System.out.println("The total meal cost is "+sum+" dollars.");
+        
+
+        
+        
+        
+    }
+}
+        
+        
+
+        
+        
+  
